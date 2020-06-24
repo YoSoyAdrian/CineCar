174d6fae793412b1c386cdf52e9829ac5895d134
YoSoyAdrian <52388238+YoSoyAdrian@users.noreply.github.com>
2020-06-23 19:21:32 -0600

    Second commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [InicioComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
