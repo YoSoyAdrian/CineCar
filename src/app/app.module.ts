f21b5fc7d448a31843a2a5e3543053c7ca64612c
YoSoyAdrian <52388238+YoSoyAdrian@users.noreply.github.com>
2020-06-23 18:54:58 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+import { MovieModule } from './movie/movie.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    ShareModule,
+    HomeModule,
+    UserModule,
+    MovieModule
   ],
   providers: [],
   bootstrap: [AppComponent]
