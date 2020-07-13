05607b0e89ad2f612791cdf1678e471b5bbd0a3a
yirlanigm <yirgm0704@gmail.com>
2020-07-13 16:53:10 -0600

    imagenes producto

--- src/app/product/product-carousel/product-carousel.component.ts
+++ src/app/product/product-carousel/product-carousel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
+import { NotificacionService } from 'src/app/share/notificacion.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-carousel',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCarouselComponent implements OnInit {
 
-  constructor() { }
-
+  datos: any;
+  error: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  constructor(private gService: GenericService,
+    private notificacion: NotificacionService) {
+  }
   ngOnInit(): void {
+  this.listaProductosTop();
+  }
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+  listaProductosTop() {
+    this.gService.list('productos/').pipe(takeUntil(this.destroy$)).
+      subscribe((data: any) => {
+        this.datos = data;
+      },
+        (error: any) => {
+          this.notificacion.mensaje(error.mensaje, error.name, 'error')
+            ;
+        }
+      );
+
   }
 
 }
